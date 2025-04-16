bed84a413ec653a54616267c4bc1d5748879b2df
Ghanshyam hadiya <160567608+ghanshyamhadiya@users.noreply.github.com>
2025-04-16 19:07:55 +0530

    Update auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -7,8 +7,8 @@ export class AuthService {
 
     constructor() {
         this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
+            .setEndpoint(import.meta.env.VITE_APPWRITE_URL) 
+            .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
         this.account = new Account(this.client)
     };
 
